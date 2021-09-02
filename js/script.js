6f8a37e693b484625a528458bd9e2b20ad540da7
sergiojanko <janko.sergio1998@gmail.com>
2021-09-02 15:32:49 +0200

    render-albums

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    albums: [],
+  },
   methods: {},
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.albums = res.data.response;
+        console.log(this.albums[0]);
+      });
+  },
 });
